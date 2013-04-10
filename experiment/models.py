a710b6d97ff14467f3fd0cdc96298af7c0331d88
Andrew Plassard <andrew.plassard@gmail.com>
2013-04-10 16:07:27 -0400

    Fixed minor bug

--- experiment/models.py
+++ experiment/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Experiment(models.Model):
-    name=model.CharField(max_length=50)
+    name=models.CharField(max_length=50)
     start_date = models.DateTimeField()
