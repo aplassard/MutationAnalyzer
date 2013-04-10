405f42cb60c834b602b8a209f63f78b76b376e04
Andrew Plassard <andrew.plassard@gmail.com>
2013-04-10 17:00:54 -0400

    Added related_name field to father and mother fields in patient

--- patient/models.py
+++ patient/models.py
@@ -5,8 +5,8 @@ class Patient(models.Model):
     experiments = models.ForeignKey(Experiment)
     name = models.CharField(max_length=200)
     affliction_status = models.BooleanField(default=False)
-    mother = models.ForeignKey('Patient',null=True,blank=True)
-    father = models.ForeignKey('Patient',null=True,blank=True)
+    mother = models.ForeignKey('Patient',null=True,blank=True,related_name='patient_mother')
+    father = models.ForeignKey('Patient',null=True,blank=True,related_name='patient_father')
 
     def __unicode__(self):
         return name
