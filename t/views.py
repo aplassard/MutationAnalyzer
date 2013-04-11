e6ab7a55f29ec2130ea80d71c3acea0dfd9f9cae
Andrew Plassard <andrew.plassard@gmail.com>
2013-04-11 13:50:47 -0400

    Updated Forms Functionality

--- t/views.py
+++ t/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
 
 def index(request):
-    return render(request, 'tests/index.html' , {})
+    if request.method == 'POST':
+        pass
+    else:
+        form = SubmitForm()
 
+    return render(request, 'test/index.html', { 'form': form })
+
+class SubmitForm(forms.Form):
+    experiment = forms.CharField(max_length=100)
+    user_name  = forms.CharField(max_length=100)
