514fd147acd7c0b916f632cce00e13cc407c9b21
Andrew Plassard <andrew.plassard@gmail.com>
2013-04-11 22:48:59 -0400

    large updates

--- MutationAnalyzer/urls.py
+++ MutationAnalyzer/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^t/',include('t.urls')),
+    url(r'^experiment/',include('experiment.urls')),
 )
